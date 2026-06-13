48947e14c3ffea56110b785217678b3b4f943d92
DominikFa <faliszekdominik@gmail.com>
2026-06-14 00:14:21 +0200

    monitoring and logging closes #17 closes #18

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,8 +1,10 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import morgan from 'morgan';
 
+import { metricsMiddleware } from './middleware/metrics_middleware';
+import { httpLogger, errorHandler } from './middleware/logger_middleware';
 import UserController from './controller/user_controller';
 import StationController from './controller/station_controller';
 import DeviceController from './controller/device_controller';
@@ -10,6 +12,8 @@ import HistoryController from './controller/history_controller';
 
 const app = express();
 
+app.use(metricsMiddleware);
+app.use(httpLogger);
 app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
@@ -27,5 +31,11 @@ app.use('/', historyController.router);
 app.get('/', (req: Request, res: Response) => {
     res.send('Rental API Running');
 });
+
+app.use((req: Request, res: Response) => {
+    res.status(404).json({ error: 'Endpoint not found' });
+});
+
+app.use(errorHandler);
 
 export default app;
